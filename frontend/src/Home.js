a4a6cb9304260ffd61d5a2b13339dcb4cd2ff55f
Krishnanunni R <krishnanunnir97@gmail.com>
2021-07-31 19:00:12 +0530

    Front end UI revamp

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,37 +1,71 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const todoItems = [{}];
 class Home extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        viewCompleted: false,
-        todoList: todoItems,
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      manhwaList: todoItems,
+    };
+  }
+  componentDidMount() {
+    this.refreshList();
+  }
 
-    render(){
-        return (
-            <main className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-6 col-md-offset-3">
-                        <h1>Find the best Manhwas</h1>
-                    </div>
-                </div>
-                <div className="mb-24">
-                    <div className="row justify-content-center">
-                        <div className="col-md-6 col-md-offset-3">
-                        <p>I rememberrrr reading Solo leveling get hooked and trying to find good Manhwas - the struggle is real 😰.
+  refreshList = () => {
+    axios
+      .get("/api/manhwa")
+      .then((res) => this.setState({ manhwaList: res.data }));
+  };
 
-    Here is a gift from me to the community</p>
-                        </div>
-                    </div>
-                </div>
-            <div>
+  renderItems = () => {
+      const itemslist = this.state.manhwaList;
+      return itemslist.map((item) => (
+        <div>
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-md-offset-3">
+              <h1>{item.name} </h1>
             </div>
-            </main>
-        )
-    }
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-md-offset-3">
+              <h1>{item.name} </h1>
+            </div>
+          </div>
+        </div>
+      ));
+
+      }
+
+  render() {
+    return (
+      <main className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 col-md-offset-3">
+            <h1>Find the best Manhwas</h1>
+          </div>
+        </div>
+        <div className="mb-24">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-md-offset-3">
+              <p>
+                I rememberrrr reading Solo leveling get hooked and trying to
+                find good Manhwas - the struggle is real 😰.
+              </p>
+              <p> Here is a gift from me to the community</p>
+            </div>
+          </div>
+        </div>
+        {this.renderItems()}
+        <div className="row justify-content-center">
+            <div className="col-md-6 col-md-offset-3">
+            </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Home;
