c60340db1084b904479c821aa32133f4135cbd5c
Krishnanunni R <krishnanunnir97@gmail.com>
2021-07-31 16:43:31 +0530

    Added basic bootstrap

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,37 @@
-function Home(){
-    return (
-        <div>
-            <h1>Find the best Manhwas</h1>
-            <p>I remember reading Solo leveling get hooked and trying to find good Manhwas - the struggle is real 😰.
+import React, { Component } from "react";
 
-                Here is a gift from me to the community</p>
-        </div>
-    )
+const todoItems = [{}];
+class Home extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        viewCompleted: false,
+        todoList: todoItems,
+      };
+    }
+
+    render(){
+        return (
+            <main className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-md-offset-3">
+                        <h1>Find the best Manhwas</h1>
+                    </div>
+                </div>
+                <div className="mb-24">
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 col-md-offset-3">
+                        <p>I rememberrrr reading Solo leveling get hooked and trying to find good Manhwas - the struggle is real 😰.
+
+    Here is a gift from me to the community</p>
+                        </div>
+                    </div>
+                </div>
+            <div>
+            </div>
+            </main>
+        )
+    }
 }
 
 export default Home;
