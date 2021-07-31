1f658c9fcf72051da55544bfb2bd6ef249579aae
Krishnanunni R <krishnanunnir97@gmail.com>
2021-07-31 14:25:19 +0530

    Making docker proper workable

---
+++ frontend/src/Home.js
@@ -0,0 +1,12 @@
+function Home(){
+    return (
+        <div>
+            <h1>Find the best Manhwas</h1>
+            <p>I remember reading Solo leveling get hooked and trying to find good Manhwas - the struggle is real 😰.
+
+                Here is a gift from me to the community</p>
+        </div>
+    )
+}
+
+export default Home;
