989aab022fa059e2130fdbccb69f595fcae9f4d6
Krishnanunni R <krishnanunnir97@gmail.com>
2021-12-18 18:33:39 +0530

    cors allow all origins

--- manhwa/settings.py
+++ manhwa/settings.py
@@ -138,10 +138,12 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = env(
-    "FRONTEND_URL", default="http://localhost:3000,http://0.0.0.0:3000"
-).split(",")
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ALLOWED_ORIGINS = env(
+#     "FRONTEND_URL", default="http://localhost:3000,http://0.0.0.0:3000"
+# ).split(",")
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
