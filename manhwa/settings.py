0427bd73469409f84f554d4cd7b4c9a02236a2a2
Krishnanunni R <krishnanunnir97@gmail.com>
2021-12-18 16:57:44 +0530

    test setting with cors allowed for all

--- manhwa/settings.py
+++ manhwa/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = env(
     "FRONTEND_URL", default="http://localhost:3000,http://0.0.0.0:3000"
 ).split(",")
